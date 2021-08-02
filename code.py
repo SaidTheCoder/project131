16f65303406a3942033fb1e2f67d0b7e5cffd224
SaidTheCoder <71198810+SaidTheCoder@users.noreply.github.com>
2021-08-01 20:58:00 -0400

    Add files via upload

---
+++ code.py
@@ -0,0 +1,8 @@
+import csv
+import pandas as pd
+df=pd.read_csv("total_stars.csv")
+print(df.columns)
+df.drop(["Unnamed: 0",'Unnamed: 6', 'Star_name.1', 'Distance.1', 'Mass.1', 'Radius.1',"Luminosity"],axis=1,inplace=True)
+print(df)
+final_data=df.dropna()
+final_data.to_csv("final_data.csv")
